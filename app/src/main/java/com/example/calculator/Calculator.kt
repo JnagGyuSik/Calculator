a3e00dc9d9aaa2ffc98f5d007553a25084ca2420
JangGyuSik <jgs288@gmail.com>
2024-03-06 20:48:17 +0900

    LV2 계산기

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -1,34 +1,43 @@
 package com.example.calculator
-
+var result:Int = 0
 fun main(){
-    var num1 = readLine()!!.toInt()
-    var calculation = readLine()!!.toString()
-    var num2 = readLine()!!.toInt()
+    val loop = true
 
-    when (calculation){
-        "+" ->  addOperation(num1,num2)
-        "-" ->  substractOperation(num1,num2)
-        "*" ->  multiplyOperation(num1,num2)
-        "/" ->  divideOperation(num1,num2)
-        else -> println("*, -, *, / 중에 입력해주세요")
+    while (loop){
+        //숫자와 연산자를 입력받음
+        var num1 = readLine()!!.toInt()
+        var calculation = readLine()!!.toString()
+        var num2 = readLine()!!.toInt()
+
+        when (calculation){
+            "+" ->  addOperation(num1,num2)
+            "-" ->  substractOperation(num1,num2)
+            "*" ->  multiplyOperation(num1,num2)
+            "/" ->  divideOperation(num1,num2)
+            else -> println("*, -, *, / 중에 입력해주세요")
+        }
     }
 }
 
+//더하기
 fun addOperation(num1:Int, num2: Int){
     var result = num1 + num2
     println("$num1 + $num2 = $result")
 }
 
+//빼기
 fun substractOperation(num1:Int, num2: Int){
     var result = num1 - num2
     println("$num1 - $num2 = $result")
 }
 
+//곱하기
 fun multiplyOperation(num1:Int, num2: Int){
     var result = num1 * num2
     println("$num1 * $num2 = $result")
 }
 
+//나누기
 fun divideOperation(num1:Int, num2: Int){
     var result = num1 / num2
     println("$num1 / $num2 = $result")
