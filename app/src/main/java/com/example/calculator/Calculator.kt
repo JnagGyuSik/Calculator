e9bb835ca395eba93e4ca1eda02ccea3ecdfed62
JangGyuSik <jgs288@gmail.com>
2024-03-07 17:15:55 +0900

    LV2 계산기 예외처리, 기능수정

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -1,20 +1,29 @@
 package com.example.calculator
-var result:Int = 0
+
 fun main(){
     val loop = true
 
+    //첫 계산이 끝나도 프로그램이 종료되지 않고 다시 처음부터 실행
     while (loop){
-        //숫자와 연산자를 입력받음
-        var num1 = readLine()!!.toInt()
+        //처음에 인트형으로 지정하고 문자열을 입력받으면 에러가남. 해결을 위해서 문자열로 지정
+        var num1 = readLine()!!.toString()
         var calculation = readLine()!!.toString()
-        var num2 = readLine()!!.toInt()
+        var num2 = readLine()!!.toString()
 
-        when (calculation){
-            "+" ->  addOperation(num1,num2)
-            "-" ->  substractOperation(num1,num2)
-            "*" ->  multiplyOperation(num1,num2)
-            "/" ->  divideOperation(num1,num2)
-            else -> println("*, -, *, / 중에 입력해주세요")
+        //예외처리
+        try {
+            var num3 = num1.toInt()
+            var num4 = num2.toInt()
+
+            when (calculation) {
+                "+" -> addOperation(num3, num4)
+                "-" -> substractOperation(num3, num4)
+                "*" -> multiplyOperation(num3, num4)
+                "/" -> divideOperation(num3, num4)
+                else -> println("연산자 *, -, *, / 중에 입력해주세요")
+            }
+        }catch (e: NumberFormatException){ //에러 NumberFormatException가 날 경우 println출력
+            println("숫자를 입력해 주세요")
         }
     }
 }
@@ -39,6 +48,6 @@ fun multiplyOperation(num1:Int, num2: Int){
 
 //나누기
 fun divideOperation(num1:Int, num2: Int){
-    var result = num1 / num2
+    var result = num1.toDouble() / num2.toDouble()
     println("$num1 / $num2 = $result")
 }
