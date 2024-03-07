126f9885e727ea4549471e77ce7ab6200e23a353
JangGyuSik <jgs288@gmail.com>
2024-03-07 20:19:02 +0900

    LV3 계산기

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -1,25 +1,16 @@
 package com.example.calculator
 
-fun main(){
-    val loop = true
-
-    //첫 계산이 끝나도 프로그램이 종료되지 않고 다시 처음부터 실행
-    while (loop){
-        //처음에 인트형으로 지정하고 문자열을 입력받으면 에러가남. 해결을 위해서 문자열로 지정
-        var num1 = readLine()!!.toString()
-        var calculation = readLine()!!.toString()
-        var num2 = readLine()!!.toString()
-
-        //예외처리
+open class Calculator(){
+    //예외처리
+    open fun calculator(num1: String, calculation:String, num2: String){
         try {
             var num3 = num1.toInt()
             var num4 = num2.toInt()
-
             when (calculation) {
-                "+" -> addOperation(num3, num4)
-                "-" -> substractOperation(num3, num4)
-                "*" -> multiplyOperation(num3, num4)
-                "/" -> divideOperation(num3, num4)
+                "+" -> AddOperation().addOperation(num3, num4)
+                "-" -> SubstractOperation().substractOperation(num3, num4)
+                "*" -> MultiplyOperation().multiplyOperation(num3, num4)
+                "/" -> DivideOperation().divideOperation(num3, num4)
                 else -> println("연산자 *, -, *, / 중에 입력해주세요")
             }
         }catch (e: NumberFormatException){ //에러 NumberFormatException가 날 경우 println출력
@@ -28,26 +19,49 @@ fun main(){
     }
 }
 
-//더하기
-fun addOperation(num1:Int, num2: Int){
-    var result = num1 + num2
-    println("$num1 + $num2 = $result")
+class AddOperation() : Calculator(){
+    //더하기
+    fun addOperation(num1:Int, num2: Int){
+        var result = num1 + num2
+        println("$num1 + $num2 = $result")
+    }
 }
 
-//빼기
-fun substractOperation(num1:Int, num2: Int){
-    var result = num1 - num2
-    println("$num1 - $num2 = $result")
+class SubstractOperation() : Calculator(){
+    //빼기
+    fun substractOperation(num1:Int, num2: Int){
+        var result = num1 - num2
+        println("$num1 - $num2 = $result")
+    }
 }
 
-//곱하기
-fun multiplyOperation(num1:Int, num2: Int){
-    var result = num1 * num2
-    println("$num1 * $num2 = $result")
+class MultiplyOperation() : Calculator(){
+    //곱하기
+    fun multiplyOperation(num1:Int, num2: Int){
+        var result = num1 * num2
+        println("$num1 * $num2 = $result")
+    }
 }
 
-//나누기
-fun divideOperation(num1:Int, num2: Int){
-    var result = num1.toDouble() / num2.toDouble()
-    println("$num1 / $num2 = $result")
+class DivideOperation() : Calculator(){
+    //나누기
+    fun divideOperation(num1:Int, num2: Int){
+        var result = num1.toDouble() / num2.toDouble()
+        println("$num1 / $num2 = $result")
+    }
+}
+
+
+fun main(){
+    val loop = true
+    //첫 계산이 끝나도 프로그램이 종료되지 않고 다시 처음부터 실행
+    while (loop){
+        //처음에 인트형으로 지정하고 문자열을 입력받으면 에러가남. 해결을 위해서 문자열로 지정
+        var num1 = readLine()!!.toString()
+        var calculation = readLine()!!.toString()
+        var num2 = readLine()!!.toString()
+
+        var myCalculator = Calculator()
+        myCalculator.calculator(num1,calculation,num2)
+    }
 }
