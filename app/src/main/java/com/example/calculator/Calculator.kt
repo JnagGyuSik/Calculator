5befc1bb4dc68dbead98307c44f2806137d7394a
JangGyuSik <jgs288@gmail.com>
2024-03-08 11:07:10 +0900

    LV4 계산기

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -4,13 +4,13 @@ open class Calculator(){
     //예외처리
     open fun calculator(num1: String, calculation:String, num2: String){
         try {
-            var num3 = num1.toInt()
-            var num4 = num2.toInt()
+            val num3 = num1.toInt()
+            val num4 = num2.toInt()
             when (calculation) {
-                "+" -> AddOperation().addOperation(num3, num4)
-                "-" -> SubstractOperation().substractOperation(num3, num4)
-                "*" -> MultiplyOperation().multiplyOperation(num3, num4)
-                "/" -> DivideOperation().divideOperation(num3, num4)
+                "+" -> AddOperation(num3,num4).operate()
+                "-" -> SubstractOperation(num3,num4).operate()
+                "*" -> MultiplyOperation(num3,num4).operate()
+                "/" -> DivideOperation(num3,num4).operate()
                 else -> println("연산자 *, -, *, / 중에 입력해주세요")
             }
         }catch (e: NumberFormatException){ //에러 NumberFormatException가 날 경우 println출력
@@ -19,34 +19,41 @@ open class Calculator(){
     }
 }
 
-class AddOperation() : Calculator(){
-    //더하기
-    fun addOperation(num1:Int, num2: Int){
-        var result = num1 + num2
+//추상화 클래스
+abstract class Operation{
+    abstract fun operate()
+}
+
+class AddOperation(val num1:Int, val num2: Int):Operation(){
+    //구현체
+    override fun operate() {
+        //더하기
+        val result = num1 + num2
         println("$num1 + $num2 = $result")
     }
 }
 
-class SubstractOperation() : Calculator(){
-    //빼기
-    fun substractOperation(num1:Int, num2: Int){
-        var result = num1 - num2
+
+class SubstractOperation(val num1:Int, val num2: Int) : Operation(){
+    override fun operate() {
+        //빼기
+        val result = num1 - num2
         println("$num1 - $num2 = $result")
     }
 }
 
-class MultiplyOperation() : Calculator(){
-    //곱하기
-    fun multiplyOperation(num1:Int, num2: Int){
-        var result = num1 * num2
+class MultiplyOperation(val num1:Int, val num2: Int) : Operation(){
+    override fun operate() {
+        //곱하기
+        val result = num1 * num2
         println("$num1 * $num2 = $result")
     }
 }
 
-class DivideOperation() : Calculator(){
-    //나누기
-    fun divideOperation(num1:Int, num2: Int){
-        var result = num1.toDouble() / num2.toDouble()
+class DivideOperation(val num1:Int, val num2: Int) : Operation(){
+    override fun operate() {
+        //나누기
+        val result = num1.toDouble() / num2.toDouble()
         println("$num1 / $num2 = $result")
     }
 }
@@ -57,11 +64,11 @@ fun main(){
     //첫 계산이 끝나도 프로그램이 종료되지 않고 다시 처음부터 실행
     while (loop){
         //처음에 인트형으로 지정하고 문자열을 입력받으면 에러가남. 해결을 위해서 문자열로 지정
-        var num1 = readLine()!!.toString()
-        var calculation = readLine()!!.toString()
-        var num2 = readLine()!!.toString()
+        val num1 = readLine()!!.toString()
+        val calculation = readLine()!!.toString()
+        val num2 = readLine()!!.toString()
 
-        var myCalculator = Calculator()
+        val myCalculator = Calculator()
         myCalculator.calculator(num1,calculation,num2)
     }
 }
